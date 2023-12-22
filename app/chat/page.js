84b946f13c14c52ad2a9cf9667191346c0330337
Michel Tsilavo <micheltsilavodev@protonmail.com>
2023-12-22 19:18:14 -0300

    my komita

---
+++ app/chat/page.js
@@ -0,0 +1,31 @@
+"use client";
+
+import { useChat } from "ai/react";
+
+export default function Chat() {
+  const { messages, input, handleInputChange, handleSubmit } = useChat();
+  return (
+    <div>
+      <div className=" px-8 py-4">
+        <p className="text-slate-100 font-semibold text-xl">Chat</p>
+      </div>
+      <div className="flex flex-col w-full max-w-md py-24 mx-auto stretch">
+        {messages.map((m) => (
+          <div key={m.id} className="whitespace-pre-wrap">
+            {m.role === "user" ? "User: " : "Wozai: "}
+            {m.content}
+          </div>
+        ))}
+
+        <form onSubmit={handleSubmit}>
+          <input
+            className="fixed bottom-0 w-full max-w-md p-2 mb-8 border border-gray-300 text-gray-800 rounded shadow-xl"
+            value={input}
+            placeholder="Say something..."
+            onChange={handleInputChange}
+          />
+        </form>
+      </div>
+    </div>
+  );
+}
